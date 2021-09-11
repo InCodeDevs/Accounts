26a519da3aac4c223767547e43627b5b9660e33e
MCTzOCK <dev@mctzock.de>
2021-09-11 11:28:21 +0200

    Fixed checkfile

--- src/module/checkfile.js
+++ src/module/checkfile.js
@@ -8,7 +8,7 @@ const path = require("path");
 
 module.exports = (p0, v0) => {
     if(!fs.existsSync(p0)) {
-        if(!fs.lstatSync(path.dirname(p0)).isDirectory()) {
+        if(!fs.existsSync(path.dirname(p0))) {
             fs.mkdirSync(path.dirname(p0))
         }
         fs.writeFileSync(p0, v0);
