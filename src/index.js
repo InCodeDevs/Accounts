22fea377d771efbe5f0c7eae2c4632f9648d854b
MCTzOCK <dev@mctzock.de>
2021-09-11 08:19:19 +0200

    [ADDED] Example ENV File

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@
  * @author Ben Siebert <ben@mctzock.de>
  * @copyright (c) 2018-2021 Ben Siebert. All rights reserved.
  */
+
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
