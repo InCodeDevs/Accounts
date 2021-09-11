2c7294e63645b0e7484941c022652410023e6dcb
MCTzOCK <dev@mctzock.de>
2021-09-11 11:17:56 +0200

    Added Shared Data

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const users = require('./module/users')
+const data = require('./module/data')
 
 app.use(cors())
 app.use(bodyParser())
@@ -104,6 +105,56 @@ app.get('/users/data', (req, res) => {
 app.get("/users/exists", (req, res) => {
     if (req.body.username) {
         res.end("{\"error\": false, \"message\": " + users.existsUser(req.body.username) + "}")
+    } else {
+        res.end("{\"error\": true, \"message\": \"Invalid Request body.\"}")
+    }
+})
+
+app.post("/data/set", (req, res) => {
+    if(req.body.username && req.body.password && req.body.key && req.body.value) {
+        res.end(JSON.stringify(
+            data.setData(req.body.username, req.body.password, req.body.key, req.body.value)
+        ))
+    } else {
+        res.end("{\"error\": true, \"message\": \"Invalid Request body.\"}")
+    }
+})
+
+app.post("/data/delete", (req, res) => {
+    if(req.body.username && req.body.password && req.body.key) {
+        res.end(JSON.stringify(
+            data.setData(req.body.username, req.body.password, req.body.key, null)
+        ))
+    } else {
+        res.end("{\"error\": true, \"message\": \"Invalid Request body.\"}")
+    }
+})
+
+app.post("/data/allow", (req, res) => {
+    if(req.body.username && req.body.password && req.body.key && req.body.newUser) {
+        res.end(JSON.stringify(
+            data.allow(req.body.username, req.body.password, req.body.key, req.body.newUser)
+        ))
+    } else {
+        res.end("{\"error\": true, \"message\": \"Invalid Request body.\"}")
+    }
+})
+
+app.post("/data/disallow", (req, res) => {
+    if(req.body.username && req.body.password && req.body.key && req.body.newUser) {
+        res.end(JSON.stringify(
+            data.disallow(req.body.username, req.body.password, req.body.key, req.body.newUser)
+        ))
+    } else {
+        res.end("{\"error\": true, \"message\": \"Invalid Request body.\"}")
+    }
+})
+
+app.get("/data/get", (req, res) => {
+    if(req.body.username && req.body.password && req.body.key) {
+        res.end(JSON.stringify(
+            data.getData(req.body.username, req.body.password, req.body.key)
+        ))
     } else {
         res.end("{\"error\": true, \"message\": \"Invalid Request body.\"}")
     }
--- src/module/checkfile.js
+++ src/module/checkfile.js
@@ -8,7 +8,9 @@ const path = require("path");
 
 module.exports = (p0, v0) => {
     if(!fs.existsSync(p0)) {
-        fs.mkdirSync(path.dirname(p0))
+        if(!fs.lstatSync(path.dirname(p0)).isDirectory()) {
+            fs.mkdirSync(path.dirname(p0))
+        }
         fs.writeFileSync(p0, v0);
     }
 }
