3f9ed8b82144f8360f5c9a100e94464605c541b0
MCTzOCK <dev@mctzock.de>
2021-10-08 17:09:20 +0200

    Added typing

---
+++ types/index.d.ts
@@ -0,0 +1,6 @@
+import accountServer = require("./lib/accounts");
+import checkFile = require("./lib/module/checkfile");
+import data = require("./lib/module/data");
+import users = require("./lib/module/users");
+import sampleApp = require("./lib/module/sampleApp");
+export { accountServer, checkFile, data, users, sampleApp };
