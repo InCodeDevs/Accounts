87a687ff129b7442b84754c914a8edc8d4007921
MCTzOCK <dev@mctzock.de>
2021-10-11 20:16:48 +0200

    Updated Types

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,5 +4,4 @@ import data = require("./lib/module/data");
 import users = require("./lib/module/users");
 import sampleApp = require("./lib/module/sampleApp");
 import postboxes = require("./lib/module/postboxes");
-import { WebClient } from "./lib/module/WebClient";
-export { accountServer, checkFile, data, users, sampleApp, postboxes, WebClient };
+export { accountServer, checkFile, data, users, sampleApp, postboxes };
